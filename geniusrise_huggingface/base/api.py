b4b4ebe4fb7d4fa892c80a13bfbfb7c47ad6997e
ixaxaar <root@ixaxaar.in>
2023-10-21 02:15:08 +0530

    add base and optimizations

---
+++ geniusrise_huggingface/base/api.py
@@ -0,0 +1,256 @@
+# 🧠 Geniusrise
+# Copyright (C) 2023  geniusrise.ai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+import cherrypy
+import torch
+import transformers
+from geniusrise import BatchInput, BatchOutput, Bolt, State
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+)
+
+
+class HuggingFaceAPI(Bolt):
+    model: Any
+    tokenizer: Any
+
+    def __init__(
+        self,
+        input: BatchInput,
+        output: BatchOutput,
+        state: State,
+    ):
+        super().__init__(input=input, output=output, state=state)
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.log.info("Loading huggingface API server")
+
+    def generate(
+        self,
+        prompt: str,
+        decoding_strategy: str = "generate",
+        **generation_params: Any,
+    ) -> dict:
+        """ """
+        results: Dict[int, Dict[str, Union[str, List[str]]]] = {}
+        eos_token_id = self.model.config.eos_token_id
+
+        # Default parameters for each strategy
+        default_params = {
+            "generate": {"max_length": 4096},
+            "greedy_search": {"max_length": 4096},
+            "contrastive_search": {"max_length": 4096},
+            "sample": {"do_sample": True, "temperature": 0.6, "top_p": 0.9, "max_length": 4096},
+            "beam_search": {"num_beams": 4, "max_length": 4096},
+            "beam_sample": {"num_beams": 4, "temperature": 0.6, "max_length": 4096},
+            "group_beam_search": {"num_beams": 4, "diversity_penalty": 0.5, "max_length": 4096},
+            "constrained_beam_search": {"num_beams": 4, "max_length": 4096, "constraints": None},
+        }
+
+        # Merge default params with user-provided params
+        strategy_params = {**default_params.get(decoding_strategy, {}), **generation_params}  # type: ignore
+
+        # Map of decoding strategy to method
+        strategy_to_method = {
+            "generate": self.model.generate,
+            "greedy_search": self.model.greedy_search,
+            "contrastive_search": self.model.contrastive_search,
+            "sample": self.model.sample,
+            "beam_search": self.model.beam_search,
+            "beam_sample": self.model.beam_sample,
+            "group_beam_search": self.model.group_beam_search,
+            "constrained_beam_search": self.model.constrained_beam_search,
+        }
+
+        try:
+            self.log.debug(f"Generating completion for prompt {prompt}")
+
+            inputs = self.tokenizer(prompt, return_tensors="pt")
+
+            if torch.cuda.is_available():
+                inputs = inputs.to("cuda:0")
+
+            input_ids = inputs["input_ids"]
+
+            # Use the specified decoding strategy
+            decoding_method = strategy_to_method.get(decoding_strategy, self.model.generate)
+            generated_ids = decoding_method(input_ids, **strategy_params)
+
+            generated_text = self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+            self.log.debug(f"Generated text: {generated_text}")
+
+            return generated_text
+
+        except Exception as e:
+            raise ValueError(f"An error occurred: {e}")
+
+    def load_huggingface_model(
+        self,
+        model_name: str,
+        model_class_name: str = "AutoModelForCausalLM",
+        tokenizer_class_name: str = "AutoTokenizer",
+        use_cuda: bool = False,
+        precision: str = "float16",
+        quantization: int = 0,
+        device_map: str | Dict | None = "auto",
+        max_memory={0: "24GB"},
+        torchscript: bool = True,
+        **model_args: Any,
+    ) -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
+        """
+        Loads a Hugging Face model and tokenizer optimized for inference.
+
+        Parameters:
+        - model_name (str): The name of the model to load.
+        - model_class_name (str): The class name of the model to load. Default is "AutoModelForCausalLM".
+        - tokenizer_class_name (str): The class name of the tokenizer to load. Default is "AutoTokenizer".
+        - use_cuda (bool): Whether to use CUDA for GPU acceleration. Default is False.
+        - precision (str): The bit precision for model and tokenizer. Options are 'float32', 'float16', 'bfloat16'. Default is 'float16'.
+        - device_map (Union[str, Dict]): Device map for model placement. Default is "auto".
+        - max_memory (Dict): Maximum GPU memory to be allocated.
+        - model_args (Any): Additional keyword arguments for the model.
+
+        Returns:
+        Tuple[AutoModelForCausalLM, AutoTokenizer]: The loaded model and tokenizer.
+
+        Usage:
+        ```python
+        model, tokenizer = load_huggingface_model("gpt-2", use_cuda=True, precision='float32', quantize=True, quantize_bits=8)
+        ```
+        """
+        self.log.info(f"Loading Hugging Face model: {model_name}")
+
+        # Determine the torch dtype based on precision
+        if precision == "float16":
+            torch_dtype = torch.float16
+        elif precision == "float32":
+            torch_dtype = torch.float32
+        elif precision == "bfloat16":
+            torch_dtype = torch.bfloat16
+        else:
+            raise ValueError("Unsupported precision. Choose from 'float32', 'float16', 'bfloat16'.")
+
+        ModelClass = getattr(transformers, model_class_name)
+        TokenizerClass = getattr(transformers, tokenizer_class_name)
+
+        # Load the model and tokenizer
+        tokenizer = TokenizerClass.from_pretrained(model_name, torch_dtype=torch_dtype)
+
+        self.log.info(f"Loading model from {model_name} with {model_args}")
+        if quantization == 8:
+            model = ModelClass.from_pretrained(
+                model_name,
+                torchscript=torchscript,
+                max_memory=max_memory,
+                device_map=device_map,
+                load_in_8bit=True,
+                **model_args,
+            )
+        elif quantization == 4:
+            model = ModelClass.from_pretrained(
+                model_name,
+                torchscript=torchscript,
+                max_memory=max_memory,
+                device_map=device_map,
+                load_in_4bit=True,
+                **model_args,
+            )
+        else:
+            model = ModelClass.from_pretrained(
+                model_name,
+                torch_dtype=torch_dtype,
+                torchscript=torchscript,
+                max_memory=max_memory,
+                device_map=device_map,
+                **model_args,
+            )
+
+        # Set to evaluation mode for inference
+        model.eval()
+
+        # Check if CUDA should be used
+        if use_cuda and torch.cuda.is_available() and device_map != "auto":
+            self.log.info("Using CUDA for Hugging Face model.")
+            model.to("cuda:0")
+
+        self.log.debug("Hugging Face model and tokenizer loaded successfully.")
+        return model, tokenizer
+
+    def listen(
+        self,
+        model_name: str,
+        model_class_name: str = "AutoModelForCausalLM",
+        tokenizer_class_name: str = "AutoTokenizer",
+        use_cuda: bool = False,
+        precision: str = "float16",
+        device_map: str | Dict | None = "auto",
+        max_memory={0: "24GB"},
+        torchscript: bool = True,
+        endpoint: str = "*",
+        port: int = 3000,
+        cors_domain: str = "http://localhost:3000",
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        **model_args: Any,
+    ) -> None:
+        self.model_name = model_name
+        self.model_class_name = model_class_name
+        self.tokenizer_class_name = tokenizer_class_name
+        self.use_cuda = use_cuda
+        self.precision = precision
+        self.device_map = device_map
+        self.max_memory = max_memory
+        self.torchscript = torchscript
+        self.model_args = model_args
+
+        self.model, self.tokenizer = self.load_huggingface_model(
+            model_name=self.model_name,
+            model_class_name=self.model_class_name,
+            tokenizer_class_name=self.tokenizer_class_name,
+            use_cuda=self.use_cuda,
+            precision=self.precision,
+            device_map=self.device_map,
+            max_memory=self.max_memory,
+            torchscript=self.torchscript,
+            **self.model_args,
+        )
+
+        def CORS():
+            cherrypy.response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+            cherrypy.response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+            cherrypy.response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+            cherrypy.response.headers["Access-Control-Allow-Credentials"] = "true"
+
+            if cherrypy.request.method == "OPTIONS":
+                cherrypy.response.status = 200
+                return True
+
+        cherrypy.config.update(
+            {
+                "server.socket_host": "0.0.0.0",
+                "server.socket_port": port,
+                "log.screen": False,
+                "tools.CORS.on": True,
+            }
+        )
+
+        cherrypy.tools.CORS = cherrypy.Tool("before_handler", CORS)
+        cherrypy.tree.mount(self, "/api/v1/", {"/": {"tools.CORS.on": True}})
+        cherrypy.tools.CORS = cherrypy.Tool("before_finalize", CORS)
+        cherrypy.engine.start()
+        cherrypy.engine.block()
---
+++ geniusrise_huggingface/base/fine_tune.py
@@ -0,0 +1,351 @@
+# 🧠 Geniusrise
+# Copyright (C) 2023  geniusrise.ai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+from abc import abstractmethod
+from typing import Dict, Optional, List
+
+import numpy as np
+from datasets import Dataset, DatasetDict
+from geniusrise import BatchInput, BatchOutput, Bolt, State
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from transformers import AutoConfig, EvalPrediction, Trainer, TrainingArguments, AutoModel
+from accelerate import infer_auto_device_map, init_empty_weights
+from peft import LoraConfig
+from trl import AutoModelForCausalLMWithValueHead
+import torch
+
+
+class HuggingFaceFineTuner(Bolt):
+    """
+    A bolt for fine-tuning Hugging Face models.
+
+    This bolt uses the Hugging Face Transformers library to fine-tune a pre-trained model.
+    It uses the `Trainer` class from the Transformers library to handle the training.
+    """
+
+    def __init__(
+        self,
+        input: BatchInput,
+        output: BatchOutput,
+        state: State,
+        **kwargs,
+    ) -> None:
+        """
+        Initialize the bolt.
+
+        Args:
+            input (BatchInput): The batch input data.
+            output (OutputConfig): The output data.
+            state (State): The state manager.
+            **kwargs: Additional keyword arguments.
+        """
+        super().__init__(input=input, output=output, state=state)
+        self.input = input
+        self.output = output
+        self.state = state
+
+        self.model_name: Optional[str] = None
+        self.tokenizer_name: Optional[str] = None
+        self.model_class: Optional[str] = None
+        self.tokenizer_class: Optional[str] = None
+        self.eval: bool = False
+
+        self.tokenizer = None
+        self.model = None
+        self.train_dataset = None
+        self.eval_dataset = None
+
+        self.log = logging.getLogger(self.__class__.__name__)
+
+    @abstractmethod
+    def load_dataset(self, dataset_path: str, **kwargs) -> Dataset | DatasetDict | Optional[Dataset]:
+        """
+        Load a dataset from a file.
+
+        Args:
+            dataset_path (str): The path to the dataset file.
+            **kwargs: Additional keyword arguments to pass to the `load_dataset` method.
+
+        Returns:
+            Dataset: The loaded dataset.
+
+        Raises:
+            NotImplementedError: This method should be overridden by subclasses.
+        """
+        raise NotImplementedError("Subclasses should implement this!")
+
+    def preprocess_data(self, **kwargs):
+        """Load and preprocess the dataset"""
+        try:
+            self.input.copy_from_remote()
+            train_dataset_path = os.path.join(self.input.get(), "train")
+            eval_dataset_path = os.path.join(self.input.get(), "eval")
+            self.train_dataset = self.load_dataset(train_dataset_path, **kwargs)
+            if self.eval:
+                self.eval_dataset = self.load_dataset(eval_dataset_path, **kwargs)
+        except Exception as e:
+            self.log.exception(f"Failed to preprocess data: {e}")
+            raise
+
+    def load_models(self, device_map: str | dict, torch_dtype: torch.dtype):
+        """Load the model and tokenizer"""
+        try:
+            # Use AutoConfig to automatically load the configuration
+            if self.model_name.lower() == "local":  # type: ignore
+                self.config = AutoConfig.from_pretrained(os.path.join(self.input.get(), "/model"))
+                self.model = getattr(__import__("transformers"), str(self.model_class)).from_pretrained(
+                    os.path.join(self.input.get(), "/model"),
+                    config=self.config,
+                    device_map=device_map,
+                    torch_dtype=torch_dtype,
+                )
+            else:
+                self.config = AutoConfig.from_pretrained(self.model_name)
+                self.model = getattr(__import__("transformers"), str(self.model_class)).from_pretrained(
+                    self.model_name,
+                    config=self.config,
+                    device_map=device_map,
+                    torch_dtype=torch_dtype,
+                )
+
+            if self.tokenizer_name.lower() == "local":  # type: ignore
+                self.tokenizer = getattr(__import__("transformers"), str(self.tokenizer_class)).from_pretrained(
+                    os.path.join(self.input.get(), "/model")
+                )
+            else:
+                self.tokenizer = getattr(__import__("transformers"), str(self.tokenizer_class)).from_pretrained(
+                    self.tokenizer_name
+                )
+        except Exception as e:
+            self.log.exception(f"Failed to load model: {e}")
+            raise
+
+    def upload_to_hf_hub(self):
+        """Upload the model and tokenizer to Hugging Face Hub."""
+        try:
+            if self.model:
+                self.model.push_to_hub(
+                    repo_id=self.hf_repo_id,
+                    commit_message=self.hf_commit_message,
+                    token=self.hf_token,
+                    private=self.hf_private,
+                    create_pr=self.hf_create_pr,
+                )
+            if self.tokenizer:
+                self.tokenizer.push_to_hub(
+                    repo_id=self.hf_repo_id,
+                    commit_message=self.hf_commit_message,
+                    token=self.hf_token,
+                    private=self.hf_private,
+                    create_pr=self.hf_create_pr,
+                )
+        except Exception as e:
+            self.log.exception(f"Failed to upload model to huggingface hub: {e}")
+            raise
+
+    def compute_metrics(self, eval_pred: EvalPrediction) -> Optional[Dict[str, float]] | Dict[str, float]:
+        """
+        Compute metrics for evaluation. This class implements a simple classification evaluation, tasks should ideally override this.
+
+        Args:
+            eval_pred (EvalPrediction): The evaluation predictions.
+
+        Returns:
+            dict: The computed metrics.
+        """
+        predictions, labels = eval_pred
+        predictions = predictions[0] if isinstance(predictions, tuple) else predictions
+        labels = labels[0] if isinstance(labels, tuple) else labels
+        predictions = np.argmax(predictions, axis=1)
+
+        return {
+            "accuracy": accuracy_score(labels, predictions),
+            "precision": precision_recall_fscore_support(labels, predictions, average="binary")[0],
+            "recall": precision_recall_fscore_support(labels, predictions, average="binary")[1],
+            "f1": precision_recall_fscore_support(labels, predictions, average="binary")[2],
+        }
+
+    def fine_tune(
+        self,
+        model_name: str,
+        tokenizer_name: str,
+        num_train_epochs: int,
+        per_device_train_batch_size: int,
+        model_class: str = "AutoModel",
+        tokenizer_class: str = "AutoTokenizer",
+        device_map: str | dict = "auto",
+        eval: bool = False,
+        hf_repo_id: Optional[str] = None,
+        hf_commit_message: Optional[str] = None,
+        hf_token: Optional[str] = None,
+        hf_private: bool = True,
+        hf_create_pr: bool = False,
+        precision: str = "",
+        quantization: Optional[int] = None,
+        lora_config: Optional[dict] = None,
+        use_accelerate: bool = False,
+        accelerate_no_split_module_classes: List[str] = [],
+        **kwargs,
+    ):
+        """
+        Fine-tune the model.
+
+        Args:
+            model_name (str): The pre-trained model name.
+            tokenizer_name (str): The pre-trained tokenizer name.
+            num_train_epochs (int): Total number of training epochs to perform.
+            per_device_train_batch_size (int): Batch size per device during training.
+            model_class (str, optional): The model class to use. Defaults to "AutoModel".
+            tokenizer_class (str, optional): The tokenizer class to use. Defaults to "AutoTokenizer".
+            eval (bool, optional): Whether to evaluate the model after training. Defaults to False.
+            hf_repo_id (str, optional): The Hugging Face repo ID. Defaults to None.
+            hf_commit_message (str, optional): The Hugging Face commit message. Defaults to None.
+            hf_token (str, optional): The Hugging Face token. Defaults to None.
+            hf_private (bool, optional): Whether to make the repo private. Defaults to True.
+            hf_create_pr (bool, optional): Whether to create a pull request. Defaults to False.
+            lora_config (dict, optional): Configuration for PEFT LoRA optimization. Defaults to None.
+            use_accelerate (bool, optional): Whether to use accelerate for distributed training. Defaults to False.
+            **kwargs: Additional keyword arguments for training.
+
+        Raises:
+            Exception: If any step in the fine-tuning process fails.
+        """
+        try:
+            # Save everything
+            self.model_name = model_name
+            self.tokenizer_name = tokenizer_name
+            self.output_dir = self.output.output_folder
+            self.num_train_epochs = num_train_epochs
+            self.per_device_train_batch_size = per_device_train_batch_size
+            self.model_class = model_class
+            self.tokenizer_class = tokenizer_class
+            self.eval = eval
+            self.hf_repo_id = hf_repo_id
+            self.hf_commit_message = hf_commit_message
+            self.hf_token = hf_token
+            self.hf_private = hf_private
+            self.hf_create_pr = hf_create_pr
+
+            # Determine the torch dtype based on precision
+            if precision == "float16":
+                torch_dtype = torch.float16
+            elif precision == "float32":
+                torch_dtype = torch.float32
+            elif precision == "bfloat16":
+                torch_dtype = torch.bfloat16
+            else:
+                raise ValueError("Unsupported precision. Choose from 'float32', 'float16', 'bfloat16'.")
+
+            if use_accelerate:
+                with init_empty_weights():
+                    model = AutoModel.from_pretrained(
+                        model_name,
+                        peft_config=lora_config,
+                        device_map=device_map,
+                    )
+                    if device_map == "float16":
+                        device_map = infer_auto_device_map(
+                            model,
+                            dtype="float16",
+                            accelerate_no_split_module_classes=accelerate_no_split_module_classes,
+                        )
+                    elif device_map == "bfloat16":
+                        device_map = infer_auto_device_map(
+                            model,
+                            dtype="bfloat16",
+                            accelerate_no_split_module_classes=accelerate_no_split_module_classes,
+                        )
+                    else:
+                        device_map = infer_auto_device_map(
+                            model,
+                            accelerate_no_split_module_classes=accelerate_no_split_module_classes,
+                        )
+
+            # Load model and tokenizer
+            # If peft_config is provided, apply PEFT optimizations
+            if lora_config and quantization and quantization > 8:
+                lora_conf = LoraConfig(**lora_config)
+                model = AutoModelForCausalLMWithValueHead.from_pretrained(
+                    model_name,
+                    peft_config=lora_config,
+                    device_map=device_map,
+                )
+            #  if PEFT config is provided and
+            elif lora_config and quantization == 8:
+                lora_conf = LoraConfig(**lora_config)
+                self.model = AutoModelForCausalLMWithValueHead.from_pretrained(
+                    model_name,
+                    load_in_8bit=True,
+                    peft_config=lora_config,
+                    device_map=device_map,
+                )
+            elif lora_config and quantization == 4:
+                lora_conf = LoraConfig(**lora_config)
+                self.model = AutoModelForCausalLMWithValueHead.from_pretrained(
+                    model_name,
+                    load_in_4bit=True,
+                    peft_config=lora_config,
+                    device_map=device_map,
+                )
+            else:
+                self.load_models(device_map=device_map, torch_dtype=torch_dtype)
+
+            # Load dataset
+            dataset_kwargs = {k.replace("data_", ""): v for k, v in kwargs.items() if "data_" in k}
+            self.preprocess_data(**dataset_kwargs)
+
+            # Separate training and evaluation arguments
+            trainer_kwargs = {k.replace("trainer_", ""): v for k, v in kwargs.items() if "trainer_" in k}
+            training_kwargs = {k.replace("training_", ""): v for k, v in kwargs.items() if "training_" in k}
+
+            # Create training arguments
+            training_args = TrainingArguments(
+                output_dir=os.path.join(self.output_dir, "model"),
+                num_train_epochs=num_train_epochs,
+                per_device_train_batch_size=per_device_train_batch_size,
+                **training_kwargs,
+            )
+
+            # Create trainer
+            trainer = Trainer(
+                model=self.model,
+                args=training_args,
+                train_dataset=self.train_dataset,
+                eval_dataset=self.eval_dataset if self.eval else None,
+                tokenizer=self.tokenizer,
+                compute_metrics=self.compute_metrics,
+                data_collator=self.data_collator if hasattr(self, "data_collator") else None,
+                **trainer_kwargs,
+            )
+
+            # Train the model
+            trainer.train()
+            trainer.save_model()
+
+            if self.eval:
+                eval_result = trainer.evaluate()
+                self.log.info(f"Evaluation results: {eval_result}")
+
+            # Save the model configuration to Hugging Face Hub if hf_repo_id is not None
+            if self.hf_repo_id:
+                self.config.save_pretrained(os.path.join(self.output_dir, "model"))
+                self.upload_to_hf_hub()
+        except Exception as e:
+            self.log.exception(f"Failed to fine tune model: {e}")
+            self.state.set_state(self.id, {"success": False, "exception": str(e)})
+            raise
+        self.state.set_state(self.id, {"success": True})
