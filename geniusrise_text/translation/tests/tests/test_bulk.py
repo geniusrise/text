2315c15784771943e34275d6ab9dcf250ede7ed3
ixaxaar <root@ixaxaar.in>
2023-11-01 01:15:29 +0530

    add fine tune for translation

---
+++ geniusrise_text/translation/tests/tests/test_bulk.py
@@ -0,0 +1,24 @@
+# 🧠 Geniusrise
+# Copyright (C) 2023  geniusrise.ai
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# # Models to test
+# models = {
+#     # fmt: off
+#     "bart": "dslim/bert-large-NER",
+#     "50": "facebook/mbart-large-50-many-to-many-mmt",
+#     "to-en": "facebook/wmt21-dense-24-wide-x-en",
+#     "from-en": "facebook/wmt21-dense-24-wide-en-x",
+#     # fmt: on
+# }
