943b6d52d57159de2e3c0527efc54f52aab479c5
ixaxaar <root@ixaxaar.in>
2023-11-09 20:58:22 +0530

    add more models to test later

--- geniusrise_text/summarization/tests/test_bulk.py
+++ geniusrise_text/summarization/tests/test_bulk.py
@@ -24,3 +24,37 @@ models = {
     "disease": "pruas/BENT-PubMedBERT-NER-Disease",
     # fmt: on
 }
+
+
+# facebook/bart-large-cnn
+# sshleifer/distilbart-cnn-12-6
+# philschmid/flan-t5-base-samsum
+# google/pegasus-xsum
+# pszemraj/led-large-book-summary
+# knkarthick/MEETING-SUMMARY-BART-LARGE-XSUM-SAMSUM-DIALOGSUM-AMI
+# ainize/kobart-news
+# human-centered-summarization/financial-summarization-pegasus
+# Callidior/bert2bert-base-arxiv-titlegen
+# pszemraj/long-t5-tglobal-base-16384-book-summary
+# Quake24/easyTermsSummerizer
+# facebook/bart-large-cnn
+
+
+# google/bigbird-pegasus-large-arxiv
+# google/bigbird-pegasus-large-bigpatent
+# google/bigbird-pegasus-large-pubmed
+# google/flan-t5-base
+# google/flan-t5-large
+# google/flan-t5-xl
+# google/flan-t5-xxl
+# google/t5-v1_1-base
+# google/t5-v1_1-large
+# google/t5-v1_1-xl
+# google/t5-v1_1-xxl
+# google/umt5-base
+# google/umt5-xl
+# google/umt5-xxl
+# google/pegasus-x-base
+# google/pegasus-large
+# google/pegasus-x-large
+# google/pegasus-multi_news
