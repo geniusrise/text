6b91f8b017d24536912f5b396f0ea14b9ea2da37
ixaxaar <root@ixaxaar.in>
2023-11-19 16:07:44 +0530

    fix imports

--- geniusrise_text/__init__.py
+++ geniusrise_text/__init__.py
@@ -12,3 +12,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from .classification import TextClassificationFineTuner
+from .classification import TextClassificationAPI
+from .classification import TextClassificationBulk
+
+from .embeddings import EmbeddingsAPI
+from .embeddings import EmbeddingsBulk
+
+from .instruction import InstructionAPI
+from .instruction import InstructionBulk
+from .instruction import TextInstructionTuningFineTuner
+
+from .language_model import LanguageModelingFineTuner
+from .language_model import LanguageModelBulk
+from .language_model import LanguageModelAPI
+
+# TODO: NER API
+from .ner import NamedEntityRecognitionBulk
+from .ner import NamedEntityRecognitionFineTuner
+
+# TODO: NLI API
+from .nli import NLIBulk
+from .nli import NLIFineTuner
+
+# TODO: QA API
+from .qa import TextQABulk
+from .qa import QuestionAnsweringFineTuner
+
+# TODO: summarization API
+from .summarization import TextSummarizationBulk
+from .summarization import SummarizationFineTuner
+
+# TODO: translation API
+from .translation import TextTranslationBulk
+from .translation import TranslationFineTuner
