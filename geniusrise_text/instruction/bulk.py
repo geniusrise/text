5ffd0767bcfd407e7ba21db210ce49528c2ab7b4
ixaxaar <root@ixaxaar.in>
2024-02-22 18:29:35 +0530

    vllm bulk working

--- geniusrise_text/instruction/bulk.py
+++ geniusrise_text/instruction/bulk.py
@@ -81,6 +81,33 @@ class InstructionBulk(TextBulk):
                 generation_max_new_tokens=100 \
                 generation_do_sample=true
     ```
+
+    or via VLLM:
+    ```bash
+    genius InstructionBulk rise \
+        batch \
+            --input_s3_bucket geniusrise-test \
+            --input_s3_folder input/chat \
+        batch \
+            --output_s3_bucket geniusrise-test \
+            --output_s3_folder output/chat \
+        none \
+        --id mistralai/Mistral-7B-Instruct-v0.1 \
+        perform_vllm \
+            --args \
+                model_name="mistralai/Mistral-7B-Instruct-v0.1" \
+                use_cuda=True \
+                precision="bfloat16" \
+                quantization=0 \
+                device_map="auto" \
+                generation_temperature=0.7 \
+                generation_top_p=1.0 \
+                generation_n=1 \
+                generation_max_tokens=50 \
+                generation_stream=false \
+                generation_presence_penalty=0.0 \
+                generation_frequency_penalty=0.0
+    ```
     """
 
     def __init__(self, input: BatchInput, output: BatchOutput, state: State, **kwargs) -> None:
